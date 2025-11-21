94dfc2d65dbce00a9ff79a738e10df1ed5f12577
jimi170 <jamshid0101www@gmail.com>
2025-11-21 17:43:18 +0500

    test3

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Test uchun text!1';
+    return 'Test uchun text!2';
   }
 }
 
